02c85c9b0d21672cbeb5dc8a79f7bf8094100a06
ctsictai <ctsictai@gmail.com>
2022-02-16 23:47:00 +0900

    ADD - model entity, schema 추기

--- app/models/foo.py
+++ app/models/foo.py
@@ -4,6 +4,8 @@ from sqlalchemy import Column, Integer, String, Boolean
 class FooItem(Base):
     __tablename__ = "foo_items"
 
-    id = Column(Integer, primary_key=True, index=True)
-    description = Column(String)
+    id = Column(Integer(unsigned=True), primary_key=True, index=True, autoincrement=True)
+    description = Column(String, nullable=True)
     public = Column(Boolean, default=False)
+    favorite_count = Column(Integer(unsigned=True), default=0)
+    name = Column(String(50), nullable=True)
--- app/schemas/foo.py
+++ app/schemas/foo.py
@@ -1,11 +1,19 @@
-## DTO 
+## DTO or Repository
 from pydantic import BaseModel
+from typing import Optional
 
 class FooItemBase(BaseModel):
-    description: str
+    description: Optional[str]
+    public : Optional[bool]
+    favorite_count: int | None = 0 
+    name : Optional[str]
+    
 
 class FooItemCreate(FooItemBase):
     public: bool
+    
+    class Config:
+        orm_mode = True
 
 class FooItem(FooItemBase):
     id: int
