6673005addfc254d11c8c959993216e7a2df18ce
ctsictai <ctsictai@gmail.com>
2022-03-22 23:54:05 +0900

    FEATURE - pytest begin

---
+++ app/tests/test_root.py
@@ -0,0 +1,11 @@
+from http import client
+from starlette.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
