e5218f1051f89e0557956fb4ff2da6e8747e8668
ctsictai <ctsictai@gmail.com>
2022-05-09 23:55:09 +0900

    TEST - crypt password and sign in user test_code ver0.1

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -3,11 +3,17 @@ from typing import Optional
 from starlette.testclient import TestClient
 
 from src.main import app
-from src.schemas.base_schemas import AllOptional
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 
 client = TestClient(app)
 
-fake_db = {"email": "test@test.com", "password": "1q2w2e3r"}
+fake_db = {
+    "email": "test@test.com", 
+    "password": "1q2w2e3r", 
+    "user_type" : "email",
+    "user_name" : "test_user",
+}
 
 
 class User(BaseModel):
@@ -17,6 +23,14 @@ class User(BaseModel):
     user_name: Optional[str]
     user_type: str
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    user_name: str | None = None
+    user_type: str | None = None
 
 def test_user_signup():
     response = client.post(
@@ -35,3 +49,26 @@ def test_user_signup():
     response_get = client.get(f"/users/{user_id}")
     assert response.json()["email"] == "test@a.com"
     assert response.json()["id"] == user_id
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def get_user(db, email:str):
+    if email in db:
+        user_dict = db[email]
+        return db
+
+def authenticate_user(fake_db, email: str, password: str):
+    user = get_user(fake_db, email)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
