b8e6dc48094d6558c789720af35b2decc9b980d0
ctsictai <ctsictai@gmail.com>
2022-05-17 01:33:21 +0900

    CONFIG - celery AMQP broker added

--- src/core/celery.py
+++ src/core/celery.py
@@ -1,7 +1,9 @@
 from celery import Celery
 
+# cloudAMQP broker and redis
 celery_task = Celery(
     "app",
+    # broker="amqps://ffddrbcd:yaGlPDnUCh6wc3aYyDQE7qCtCsFTu8fN@dingo.rmq.cloudamqp.com/ffddrbcd",
     broker="redis://127.0.0.1:6379/0",
     include=['src.core.celery_worker'],
     result_backend="rpc://",
